f7bb8b9c5558509e2862127ef6031e644c3d1666
John <john.kuronya@outlook.com>
2025-03-23 02:09:40 -0400

    Second Commit

--- week-1/project08-01/project08-01_txt.js
+++ week-1/project08-01/project08-01_txt.js
@@ -4,16 +4,47 @@
       Project 08-01
 
       Project to create a timer object
-      Author: 
-      Date:   
+      Author: John Kuronya
+      Date: 3/17/25
 
       Filename: project08-01.js
 */
 
 /*--------------- Object Code --------------------*/
+function timer(min, sec) {
+  timer.minutes = min,
+  timer.seconds = sec,
+  timer.timeID = null,
 
+  //Method to run/pause timer
+  timer.prototype.RunPause = function(timer, minBox, secBox) {
+        if(this.timeID) {
+              window.clearInterval(this.timeID);
+              this.timeID = null;
 
+        } else {
+              this.timeID = window.setInterval(countdown, 1000);
+        }
+  }
 
+  //Countdown function
+  function countdown() {
+        if (timer.seconds > 0) {
+              timer.seconds = timer.seconds -= 1;
+
+        } else if (timer.minutes > 0) {
+              timer.minutes = timer.minutes -= 1;
+              timer.seconds = 59;
+        } else {
+              window.clearInterval(timer.timeID);
+              timer.timeID = null;
+        }
+
+        minBox.value = timer.minutes;
+        secBox.value = timer.seconds;
+  }
+
+}
 
 
 
@@ -25,3 +56,16 @@ let minBox = document.getElementById("minutesBox");
 let secBox = document.getElementById("secondsBox");
 let runPauseTimer = document.getElementById("runPauseButton");
 
+let myTimer = new timer(minBox.value, secBox.value);
+
+minBox.onchange = function() {
+      myTimer.minutes = minBox.value;
+}
+
+secBox.onchange = function() {
+      myTimer.seconds = secBox.value;
+}
+
+runPauseTimer.onclick = function() {
+      myTimer.RunPause(myTimer, minBox, secBox);
+}
